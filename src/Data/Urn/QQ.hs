ec9724e21afdfdac293d3f78c6c4622f197d38ba
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2016-06-04 00:33:04 -0400

    Refactored the `QQ` module

--- src/Data/Urn/QQ.hs
+++ src/Data/Urn/QQ.hs
@@ -1,62 +1,12 @@
 {-# LANGUAGE LambdaCase, TemplateHaskell #-}
 
-module Data.Urn.QQ where
+module Data.Urn.QQ (urn) where
 
-import Data.Traversable
-
-import Language.Haskell.TH
 import Language.Haskell.TH.Quote
-import Language.Haskell.Meta.Parse
-
-import Data.Urn.Internal
-
-embedWord :: Word -> Exp
-embedWord = LitE . IntegerL . toInteger
-
-embedWeight :: Weight -> Exp
-embedWeight = embedWord
-
-embedSize :: Size -> Exp
-embedSize (Size s) = ConE 'Size `AppE` embedWord s
-
-embedBTree :: BTree Exp -> Exp
-embedBTree (BLeaf a)   = ConE 'BLeaf `AppE` a
-embedBTree (BNode l r) = ConE 'BNode `AppE` embedWTree l `AppE` embedWTree r
-               
-embedWTree :: WTree Exp -> Exp
-embedWTree wt =
-  RecConE 'WTree [ ('weight, embedWeight $ weight wt)
-                 , ('btree,  embedBTree  $ btree  wt) ]
-
-embedTree :: Urn Exp -> Exp
-embedTree u = RecConE 'Urn [ ('size,  embedSize  $ size  u)
-                           , ('wtree, embedWTree $ wtree u) ]
-
--- We don't handle extensions
-parseUrnList :: String -> Either String [(Word, Exp)]
-parseUrnList str =
-  case parseExp $ "[" ++ str ++ "]" of
-    Left  _ ->
-      Left "Parse error in urn"
-    Right (ListE tups) ->
-      for tups $ \case
-        TupE [LitE (IntegerL w), e] | toInteger (minBound :: Word) <= w
-                                    , w <= toInteger (maxBound :: Word) ->
-          Right (fromInteger w :: Word, e)
-        TupE [_, _] ->
-          Left $ "A weighted pair in this urn lacked a valid literal weight"
-        _ ->
-          Left $ "This urn contained a non-pair element"
-    Right _ ->
-      Left "This urn does not contain a list of pairs"
-
-parseUrn :: String -> Either String (Urn Exp)
-parseUrn str = (fromList <$> parseUrnList str) >>= \case
-                 Just urn -> Right urn
-                 Nothing  -> Left  "Empty urn"
+import Data.Urn.QQ.ParseExp
 
 urn :: QuasiQuoter
-urn = QuasiQuoter { quoteExp  = either fail (pure . embedTree) . parseUrn
+urn = QuasiQuoter { quoteExp  = either fail (pure . urnExp) . parseUrn
                   , quotePat  = unsupported "patterns"
                   , quoteType = unsupported "types"
                   , quoteDec  = unsupported "declarations" }
---
+++ src/Data/Urn/QQ/ParseExp.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE LambdaCase, TemplateHaskell #-}
+
+module Data.Urn.QQ.ParseExp (
+  -- * Parse a literal 'Urn'
+  parseUrnList, parseUrn,
+  -- * Lift components of 'Urn's straight to 'Exp's (without 'Q')
+  wordExp, weightExp, sizeExp,
+  btreeExp, wtreeExp,
+  urnExp
+  ) where
+
+import Data.Traversable
+
+import Language.Haskell.TH
+import Language.Haskell.Meta.Parse
+
+import Data.Urn.Internal
+
+-- We don't handle extensions
+parseUrnList :: String -> Either String [(Word, Exp)]
+parseUrnList str =
+  case parseExp $ "[" ++ str ++ "]" of
+    Left  _ ->
+      Left "Parse error in urn"
+    Right (ListE tups) ->
+      for tups $ \case
+        TupE [LitE (IntegerL w), e] | toInteger (minBound :: Word) <= w
+                                    , w <= toInteger (maxBound :: Word) ->
+          Right (fromInteger w :: Word, e)
+        TupE [_, _] ->
+          Left $ "A weighted pair in this urn lacked a valid literal weight"
+        _ ->
+          Left $ "This urn contained a non-pair element"
+    Right _ ->
+      Left "This urn does not contain a list of pairs"
+
+parseUrn :: String -> Either String (Urn Exp)
+parseUrn str = (fromList <$> parseUrnList str) >>= \case
+                 Just urn -> Right urn
+                 Nothing  -> Left  "Empty urn"
+
+wordExp :: Word -> Exp
+wordExp = LitE . IntegerL . toInteger
+
+weightExp :: Weight -> Exp
+weightExp = wordExp
+
+sizeExp :: Size -> Exp
+sizeExp (Size s) = ConE 'Size `AppE` wordExp s
+
+btreeExp :: BTree Exp -> Exp
+btreeExp (BLeaf a)   = ConE 'BLeaf `AppE` a
+btreeExp (BNode l r) = ConE 'BNode `AppE` wtreeExp l `AppE` wtreeExp r
+               
+wtreeExp :: WTree Exp -> Exp
+wtreeExp wt = RecConE 'WTree [ ('weight, weightExp $ weight wt)
+                             , ('btree,  btreeExp  $ btree  wt) ]
+
+urnExp :: Urn Exp -> Exp
+urnExp u = RecConE 'Urn [ ('size,  sizeExp  $ size  u)
+                        , ('wtree, wtreeExp $ wtree u) ]
